ebb2d2f5ab2d98c452ce00ef2d2e664f80d49c2c
victorlirafront <pj.victor@wine.com.br>
2023-06-11 17:04:51 -0300

    feat: adding posts into the database

--- backend/server.ts
+++ backend/server.ts
@@ -1,14 +1,26 @@
 import express, { Request, Response } from "express";
 import db from "./config/db";
+import cors from 'cors'
 
 const app = express();
+
 const port = 3001;
 
-app.get("/", (req: Request, res: Response) => {});
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send('home')
+});
 
 app.post("/api/create", (req: Request, res: Response) => {
+  const username = req.body.userName;
+  const title = req.body.title;
+  const text = req.body.text;
+
   db.query(
-    "INSERT INTO posts (title, post_text, user_name) VALUES ()",
+    "INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)", [title, text, username],
     (err, result) => {
       if (err) {
         console.log(err);
