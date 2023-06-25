d87468f141890ce7f68866f264bc5d6a6c97d8a3
victorlirafront <pj.victor@wine.com.br>
2023-06-24 21:13:00 -0300

    formatting data

--- frontend/src/components/Post/Post.styled.ts
+++ frontend/src/components/Post/Post.styled.ts
@@ -4,7 +4,7 @@ export const StyledPost = styled.div`
     color: #000;
     background: #fff;
     border: 1px solid red;
-    max-width: 400px;
+    max-width: 800px;
     margin: 0 auto;
     text-align: center;
     text-decoration: none;
--- frontend/src/components/Post/index.tsx
+++ frontend/src/components/Post/index.tsx
@@ -6,7 +6,7 @@ const Post = function (props: any) {
         <Link href={`Posts/${props.id}`} style={{textDecoration: 'none'}}>
             <StyledPost>
                 <h1>{props.title}</h1>
-                <p>{props.post_text}</p>
+                <p>{props.post_text.length > 500 ? props.post_text.substring(0,500) + ' ...' : props.post_text}</p>
                 <h4>{props.user_name}</h4>
             </StyledPost>
         </Link>
