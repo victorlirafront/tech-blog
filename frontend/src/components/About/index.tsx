6b691adc91b96c2b7e2e4cd2c874c68867643ffd
victorlirafront <pj.victor@wine.com.br>
2023-06-25 23:51:25 -0300

    vortech web

--- frontend/src/components/About/index.tsx
+++ frontend/src/components/About/index.tsx
@@ -4,7 +4,7 @@ const About = function () {
     return (
         <StyledAbout>
             <div className="about-wrapper">
-                <h1 className='about-title'>Blog da Vortex</h1>
+                <h1 className='about-title'>Blog da Vortech Web</h1>
                 <p className='about-text'>
                     Junte-se a milhares de devs e acelere na direção dos seus
                     objetivos. Transforme sua carreira em uma jornada de evolução
