3f7411dce72193f5721445ed55ed40fe4e9a4372
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2023-08-12 10:18:31 -0300

    api port updated

--- backend/server.ts
+++ backend/server.ts
@@ -42,6 +42,6 @@ app.post("/api/create", (req: Request, res: Response) => {
   );
 });
 
-app.listen(process.env.API_PORT, () => {
-  console.log(`Server is running on port ${process.env.API_PORT}`);
+app.listen(3001, () => {
+  console.log(`Server is running on port ${3001}`);
 });
