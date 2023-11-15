741b1ccb6f36bff19af08b727cd3b4a9e13a76dc
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2023-11-15 15:18:45 -0300

    feat: category

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -19,7 +19,14 @@ const Header = function () {
                             <Link href="/">Home</Link>
                         </li>
                         <li>
-                            Category
+                            <select name="" id="">
+                                <option disabled value="javascript">Category</option>
+                                <option value="Javascript">Javascript</option>
+                                <option value="Javascript">React</option>
+                                <option value="Javascript">Next</option>
+                                <option value="Javascript">HTML</option>
+                                <option value="Javascript">CSS</option>
+                            </select>
                         </li>
                     </ul>
                 </nav>
