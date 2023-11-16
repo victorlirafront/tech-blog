34f2b78736a1966d5279fbe2619f0e440ebab9eb
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2023-11-16 12:35:16 -0300

    fix: pagination category

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -2,6 +2,7 @@ import StyledPagination from "./Pagination.styled";
 import Link from "next/link";
 import { GlobalContext } from '../../Context/pagination';
 import { useContext, useEffect } from "react";
+import { useRouter } from 'next/router';
 
 interface IpropsPagination {
     pageLength: number
@@ -10,8 +11,15 @@ interface IpropsPagination {
     hasPreviousPage: boolean
 }
 
+
 const Pagination = function(props: IpropsPagination){
     let { setPage, page, setTotalPages } = useContext(GlobalContext);
+    const router = useRouter();
+    let category = router.query.category;
+
+    if(category === undefined) {
+        category = "all"
+    }
 
     function setNextPage() {
         let nextPage = page += 1
@@ -27,7 +35,7 @@ const Pagination = function(props: IpropsPagination){
     const createNextPageArrow = function(){
         if(props.hasNextPage){
             return(
-                <Link onClick={() => setNextPage()} href={`/Pagination/${page}`}>
+                <Link onClick={() => setNextPage()} href={`/Pagination/${page}?category=${category}`}>
                     <div className="icon-arrow icon-arrow-right"> 
                         <img src="https://ik.imagekit.io/Victorliradev/blog_pessoal/assets/arrow-right_eVbRRghk9.png?updatedAt=1696390413993" alt="" />
                     </div>
@@ -39,7 +47,7 @@ const Pagination = function(props: IpropsPagination){
     const createPreviousPageArrow = function(){
         if(props.hasPreviousPage){
             return(
-                <Link onClick={() => setPreviowPage()} href={`/Pagination/${page -2}`} >
+                <Link onClick={() => setPreviowPage()} href={`/Pagination/${page -2}?category=${category}`} >
                     <div className="icon-arrow icon-arrow-left">
                         <img src="https://ik.imagekit.io/Victorliradev/blog_pessoal/assets/arrow-right_eVbRRghk9.png?updatedAt=1696390413993" alt="" />
                     </div>
