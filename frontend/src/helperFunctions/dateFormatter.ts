67b667176dbf85729ae9a551a1654ca2948a7131
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2023-12-14 23:55:30 -0300

    refactor: fixing indentation code

--- frontend/src/helperFunctions/dateFormatter.ts
+++ frontend/src/helperFunctions/dateFormatter.ts
@@ -1,14 +1,14 @@
 function dateFormatter(input: string) {
-  const [dia, mes, ano] = input.split('/');
-  const meses = [
-    'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
-    'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
-  ];
+    const [dia, mes, ano] = input.split('/');
+    const meses = [
+        'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
+        'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
+    ];
 
-  const mesFormatado = meses[parseInt(mes) - 1];
+    const mesFormatado = meses[parseInt(mes) - 1];
 
-  const dataFormatada = `${dia} de ${mesFormatado} de ${ano}`;
-  return dataFormatada;
+    const dataFormatada = `${dia} de ${mesFormatado} de ${ano}`;
+    return dataFormatada;
 }
 
 export default dateFormatter
