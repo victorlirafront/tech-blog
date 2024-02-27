26f6268e47693901b9558c709ead7a63d5061dbe
VictorLira-DEV <victorliradev@gmail.com>
2024-02-27 00:27:55 -0300

    feat: conditional rendering

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,35 +1,59 @@
-import  StyledFooter from './Footer.styled'
+import StyledFooter from './Footer.styled';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
-const getCurrentYear = function(){
-    const dataAtual = new Date();
-    // Obtém o ano atual
-    return dataAtual.getFullYear();
-}
+const getCurrentYear = () => new Date().getFullYear();
 
-const Footer = function(){
-    return (
-        <StyledFooter>
-            <div className='icons-wrapper'>
-                <Link href="https://github.com/victorlirafront" target="_blank">
-                    <div className='icons-group'>
-                        <Image loading="lazy" width={30} height={30} alt="teste" className='icon' src="https://ik.imagekit.io/Victorliradev/blog_pessoal/assets/github_LDTcy1bc_.png?updatedAt=16886127797" />
-                    </div>
-                </Link>
+const Footer = () => {
+   const [shouldRender, setShouldRender] = useState(false);
 
-                <Link href="https://www.linkedin.com/in/victor-lira-front-end/" target="_blank">
-                    <div className='icons-group'>
-                        <Image loading="lazy" width={30} height={30} alt="teste" className='icon' src="https://ik.imagekit.io/Victorliradev/blog_pessoal/assets/linkedin_aDQr6Huy2.png?updatedAt=16886127938" />
-                    </div>
-                </Link>
+   useEffect(() => {
+      setShouldRender(true);
+   }, []);
+
+   const githubUrl = "https://github.com/victorlirafront";
+   const linkedinUrl = "https://www.linkedin.com/in/victor-lira-front-end/";
+   const githubImageSrc = "https://ik.imagekit.io/Victorliradev/blog_pessoal/assets/github_LDTcy1bc_.png?updatedAt=16886127797";
+   const linkedinImageSrc = "https://ik.imagekit.io/Victorliradev/blog_pessoal/assets/linkedin_aDQr6Huy2.png?updatedAt=16886127938";
+
+   return (
+      shouldRender ? (
+         <StyledFooter>
+            <div className="icons-wrapper">
+               <Link href={githubUrl} target="_blank">
+                  <div className="icons-group">
+                     <Image
+                        loading="lazy"
+                        width={30}
+                        height={30}
+                        alt="GitHub"
+                        className="icon"
+                        src={githubImageSrc}
+                     />
+                  </div>
+               </Link>
+
+               <Link href={linkedinUrl} target="_blank">
+                  <div className="icons-group">
+                     <Image
+                        loading="lazy"
+                        width={30}
+                        height={30}
+                        alt="LinkedIn"
+                        className="icon"
+                        src={linkedinImageSrc}
+                     />
+                  </div>
+               </Link>
             </div>
 
-            <div className='direitos-autorais'>
-                <p>All rights reserved to © Victor Lira {getCurrentYear()}</p>
+            <div className="direitos-autorais">
+               <p>All rights reserved to © Victor Lira {getCurrentYear()}</p>
             </div>
-        </StyledFooter>
-    )
-}
+         </StyledFooter>
+      ) : <div></div>
+   )
+};
 
-export default Footer
+export default Footer;
