aa550729e796a98aeb7f88b2ea262e6de11b926a
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-09-19 17:54:41 -0300

    feat: remove render condition

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,63 +1,52 @@
 import StyledFooter from './Footer.styled';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 import React from 'react';
 import { GITHUB_ICON, LINKEDIN_ICON } from '@/constants/images';
+import { GITHUB_LINk, LINKEDIN_LINK } from '@/constants/links';
 
 const getCurrentYear = () => new Date().getFullYear();
 
 const Footer = () => {
-  const [shouldRender, setShouldRender] = useState(false);
-
-  useEffect(() => {
-    setShouldRender(true);
-  }, []);
-
-  const githubUrl = 'https://github.com/victorlirafront';
-  const linkedinUrl = 'https://www.linkedin.com/in/victor-lira-front-end/';
-
   return (
-    <React.Fragment>
-      {shouldRender ? (
-        <StyledFooter>
-          <div className="icons-wrapper">
-            <Link href={githubUrl} target="_blank">
-              <div className="icons-group">
-                <Image
-                  loading="lazy"
-                  width={30}
-                  height={30}
-                  alt="GitHub"
-                  className="icon"
-                  src={GITHUB_ICON}
-                />
-              </div>
-            </Link>
-
-            <Link href={linkedinUrl} target="_blank">
-              <div className="icons-group">
-                <Image
-                  loading="lazy"
-                  width={30}
-                  height={30}
-                  alt="LinkedIn"
-                  className="icon"
-                  src={LINKEDIN_ICON}
-                />
-              </div>
-            </Link>
+    <StyledFooter>
+      <div className="icons-wrapper">
+        <Link href={GITHUB_LINk} target="_blank">
+          <div className="icons-group">
+            <Image
+              loading="lazy"
+              width={30}
+              height={30}
+              alt="GitHub"
+              className="icon"
+              src={GITHUB_ICON}
+            />
           </div>
+        </Link>
 
-          <div className="direitos-autorais">
-            <p>Todos os direitos reservados © {getCurrentYear()} <strong>Victor Lira</strong> </p>
-            <p>Blog criado com <strong>Next.js</strong></p>
+        <Link href={LINKEDIN_LINK} target="_blank">
+          <div className="icons-group">
+            <Image
+              loading="lazy"
+              width={30}
+              height={30}
+              alt="LinkedIn"
+              className="icon"
+              src={LINKEDIN_ICON}
+            />
           </div>
-        </StyledFooter>
-      ) : (
-        <div></div>
-      )}{' '}
-    </React.Fragment>
+        </Link>
+      </div>
+
+      <div className="direitos-autorais">
+        <p>
+          Todos os direitos reservados © {getCurrentYear()} <strong>Victor Lira</strong>
+        </p>
+        <p>
+          Blog criado com <strong>Next.js</strong>
+        </p>
+      </div>
+    </StyledFooter>
   );
 };
 
---
+++ frontend/src/constants/links.ts
@@ -0,0 +1,2 @@
+export const GITHUB_LINk = 'https://github.com/victorlirafront';
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/victor-lira-front-end/'
