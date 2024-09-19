fedbf0de2bfd78d555c1bc3ebf017968d3311e33
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-09-19 20:50:43 -0300

    test: testing About component

---
+++ frontend/src/components/About/index.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import About from './index';
+
+describe('About', () => {
+  it('renders About component', () => {
+    render(<About />);
+  });
+
+  it("render About title", () => {
+    render(<About />);
+    const heading = screen.getByRole("heading", {name: /Artigos/i })
+    expect(heading).toBeInTheDocument();
+  })
+});
