b84f7d5d8e75f1399523b2fa32f4430aa312740a
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-10-06 07:25:46 -0300

    chore: add localhost

--- frontend/src/constants/postsEndPoints.ts
+++ frontend/src/constants/postsEndPoints.ts
@@ -1,4 +1,5 @@
 export const postsEndPoints = [
+  'http://localhost:3001',
   'https://blog-backend-tau-three.vercel.app',
   'https://blog-backend-g9k4y75fk-victorlirafront.vercel.app',
   'https://blog-tau-rosy-55.vercel.app',
