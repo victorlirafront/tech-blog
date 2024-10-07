ceae7ce53cca565a19ed40c88f937e0ec08bd5a0
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-10-07 20:06:37 -0300

    test: fireEvent onclick function

---
+++ frontend/src/components/FormModal/index.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import FormModal from '.';
+
+describe('FormModal', () => {
+  const onCloseFormModal = jest.fn(); // função mock definida corretamente
+
+  it('Should render form modal', () => {
+    // Aqui passamos o mock corretamente como prop para o componente
+    const { getByText, queryByText, getByRole } = render(<FormModal className="teste" onCloseFormModal={onCloseFormModal} />);
+    
+    // Testa se o texto esperado está na tela
+    expect(getByText('As suas informações foram enviadas com sucesso!')).toBeInTheDocument();
+
+    // Verifica se um texto inválido não está presente
+    expect(queryByText('Modal invalido')).not.toBeInTheDocument();
+
+    // Verifica se o botão está presente
+    expect(getByRole("button")).toBeInTheDocument();
+
+    // Simula o clique no botão
+    const button = screen.getByText("ok");
+    fireEvent.click(button);
+
+    // Verifica se a função onCloseFormModal foi chamada
+    expect(onCloseFormModal).toHaveBeenCalled();
+  });
+});
