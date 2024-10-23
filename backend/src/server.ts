3c29401029fb2dac7e3503a41a6acd026f0b7e3b
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-10-23 07:33:07 -0300

    fix: handle cors error 1

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,32 @@
 import express from 'express';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 import emailRoutes from './routes/emailRoutes';
 import getProducts from './routes/getProducts';
 import createPost from './routes/createPost';
 
+const allowedOrigins: string[] = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://www.victorlirablog.com' ,
+  'https://blog-backend-tau-three.vercel.app',
+  'https://blog-backend-g9k4y75fk-victorlirafront.vercel.app',
+  'https://blog-tau-rosy-55.vercel.app',
+  'https://blog-git-main-victorlirafront.vercel.app',
+];
+
+const corsOptions: CorsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
 const app = express();
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'https://blog-backend-tau-three.vercel.app', 
-    'https://blog-backend-g9k4y75fk-victorlirafront.vercel.app',
-    'https://blog-tau-rosy-55.vercel.app',
-    'https://blog-git-main-victorlirafront.vercel.app'
-  ], 
-  methods: ['GET', 'POST', 'OPTIONS'],
-}));
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/', emailRoutes);
