5433d6d091f799de2cea76aece40a54bb8a02788
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-10-23 09:10:53 -0300

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,40 @@
 import express from 'express';
-import cors, { CorsOptions } from 'cors';
 import emailRoutes from './routes/emailRoutes';
 import getProducts from './routes/getProducts';
 import createPost from './routes/createPost';
 
+import cors, { CorsOptions } from 'cors';
+
+const allowedOrigins: string[] = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://blog-backend-tau-three.vercel.app',
+  'https://blog-backend-g9k4y75fk-victorlirafront.vercel.app',
+  'https://blog-tau-rosy-55.vercel.app',
+  'https://blog-git-main-victorlirafront.vercel.app',
+  'https://www.victorlirablog.com',
+];
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  credentials: true
-}))
-
+app.use(cors(corsOptions));
 app.use(express.json());
 
+// Suas rotas
 app.use('/api/', emailRoutes);
 app.use('/api/', getProducts);
 app.use('/api/', createPost);
