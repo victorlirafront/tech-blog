a0ea2942976e8abae20228fa9135973fee749dbc
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-10-23 00:00:14 -0300

    .

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,17 @@ import getProducts from './routes/getProducts';
 import createPost from './routes/createPost';
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://blog-backend-tau-three.vercel.app', 
+    'https://blog-backend-g9k4y75fk-victorlirafront.vercel.app',
+    'https://blog-tau-rosy-55.vercel.app',
+    'https://blog-git-main-victorlirafront.vercel.app'
+  ], 
+  methods: ['GET', 'POST', 'OPTIONS'],
+}));
 app.use(express.json());
 
 app.use('/api/', emailRoutes);
