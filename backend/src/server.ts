6e8a8f81869c6960d84cb4e279a63bb3bdd08109
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-10-23 09:14:20 -0300

    remove allowedHeaders

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,7 +24,6 @@ const corsOptions: CorsOptions = {
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   optionsSuccessStatus: 204,
 };
