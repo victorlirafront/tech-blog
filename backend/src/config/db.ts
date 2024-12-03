08ea019fb5ac6caa40356f05421b0b54d5a81a96
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-03 03:55:08 -0300

    fix: update database PORT

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,4 +1,4 @@
-import mysql from 'mysql2/promise'; // Note que estou usando a versão 'promise' do mysql2
+import mysql from 'mysql2/promise';
 require('dotenv').config();
 
 export const pool = mysql.createPool({
@@ -7,23 +7,15 @@ export const pool = mysql.createPool({
   password: process.env.PASSWORD!,
   database: process.env.DATABASE!,
   port: parseInt(process.env.PORT!, 10),
-  waitForConnections: true, // Espera por uma conexão disponível, caso o pool esteja cheio
-  connectionLimit: 10, // Define o número máximo de conexões no pool
-  queueLimit: 0, // Sem limite para a fila de conexões
-});
-
-console.log({
-  host: process.env.HOST,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 pool.getConnection()
   .then(connection => {
     console.log('Connected successfully to the database!');
-    connection.release(); // Libera a conexão de volta ao pool
+    connection.release();
   })
   .catch(err => {
     console.error('Error connecting to the database:', err);
--- frontend/src/constants/endpoints.ts
+++ frontend/src/constants/endpoints.ts
@@ -1,2 +1,2 @@
-export const DEV_API_URL = 'http://localhost:3306';
+export const DEV_API_URL = 'http://localhost:20787';
 export const PROD_API_URL = 'https://tech-blog-production-cd77.up.railway.app';
