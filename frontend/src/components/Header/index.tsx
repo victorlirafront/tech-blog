aac36a87012770259b6ab89336137ae766a82127
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-08 01:50:32 -0300

    update validation

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -50,7 +50,7 @@ const Header = function (props: IHeaderProps) {
   }, [menuToggleBaseOnUrl]);
 
   const showMobileMenu = function () {
-    props.setOpenMobileMenu(true);
+    props.setOpenMobileMenu?.(true);
   };
 
   const categoryOptionHandler = function (route: string, category: string) {
@@ -75,7 +75,7 @@ const Header = function (props: IHeaderProps) {
   };
 
   const hideMobileMenu = function () {
-    props.setOpenMobileMenu(false);
+    props.setOpenMobileMenu?.(false);
   };
 
   const goBackToHomePage = function () {
