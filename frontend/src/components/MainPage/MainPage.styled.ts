fa9a2a71074a65434c97d42b7c8ac4df541a62fa
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2024-12-08 23:14:36 -0300

    update

--- frontend/src/components/MainPage/MainPage.styled.ts
+++ frontend/src/components/MainPage/MainPage.styled.ts
@@ -2,7 +2,7 @@
 import styled from 'styled-components';
 
 const StyledMainPage = styled.div`
-  padding: 0 60px 60px 60px;
+  padding: 0 0 60px 0;
   background: #000;
   max-width: 1920px;
   margin: 0 auto;
